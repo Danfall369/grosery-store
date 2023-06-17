5291c359395f125fca5f7f93adce35842c1daca2
Daniel Falcon <danfall963@gmail.com>
2023-06-17 01:52:28 -0400

    Adding Layout, Navbar and Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { AiFillFacebook, AiFillInstagram, AiOutlineTwitter } from 'react-icons/ai'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="footer-container">
+      <p> 2023 Multitienda El Amiguito Forever, C.A.</p>
+      <p className="icons">
+        <AiFillFacebook />
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
   )
 }
 
