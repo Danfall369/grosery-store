da29e3387a3efc8f88ce12f46c20cbf84d611bc1
Daniel Falcon <danfall963@gmail.com>
2023-06-17 04:01:24 -0400

    adding context logic for Quantity

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import Link from 'next/link'
 import { AiOutlineShopping } from 'react-icons/ai'
+import Image from 'next/image'
+import Logo from '../public/logo.png';
 
 const Navbar = () => {
   return (
     <div className="navbar-container">
       <p className='logo'>
+        <Image src={Logo} alt="Logo" width={30} ></Image>
         <Link href="/">Multitiendas E.A.F</Link>
       </p>
 
